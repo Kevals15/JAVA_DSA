91f3a88f12ffb3dc2e114e26c940dc53bc14dc4a
Kevals15 <keval.shah1507@gmail.com>
2025-07-12 02:18:31 +0530

    refactor ValidAnagram: simplify isAnagram method and clean up commented code

--- String/easy/ValidAnagram.java
+++ String/easy/ValidAnagram.java
@@ -8,25 +8,44 @@ public class ValidAnagram {
         
     }
 
-    // 
-    
-    // second solution
-    public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
+    // public static boolean isAnagram(String s, String t) {
+    //     if(s.length()!=t.length()) return false;
+    //     HashMap<Character,Integer> map = new HashMap<>();
+    //     for(int i=0;i<s.length();i++)
+    //     {
+    //         map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+    //     }
+    //     for(int i=0;i<t.length();i++)
+    //     {
+    //         map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)-1);
+    //     }
+    //     for(Map.Entry<Character,Integer> set : map.entrySet())
+    //     {
+    //         if(set.getValue()!=0)
+    //         {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 
-        int[] count = new int[26];
+
+
+    //second approach
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26]; // Assuming only lowercase letters a-z
+
         for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
         }
 
         for (int c : count) {
-            if (c != 0) {
-                return false;
-            }
+            if (c != 0) return false;
         }
+
         return true;
     }
 }
